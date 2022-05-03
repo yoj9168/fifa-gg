cd43b4021fae927cecf7982dc62b9713c68c67ca
yoj9168@gmail.com <yoj9168@gmail.com>
2022-05-03 21:12:48 +0900

    20220503

--- indexJs.js
+++ indexJs.js
@@ -29,9 +29,7 @@ function get_divisionName(diVison){
 }
 
 $('.submit').click(function(){
-    //var nickname;
     var nickname = $('.input').val();
-    //nickname = "5호선파브르"
     fetch("https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname="+nickname,{
         method:'GET',
         headers:{"Authorization":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMjY4OTE3Mjk2IiwiYXV0aF9pZCI6IjIiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4iLCJzZXJ2aWNlX2lkIjoiNDMwMDExNDgxIiwiWC1BcHAtUmF0ZS1MaW1pdCI6IjUwMDoxMCIsIm5iZiI6MTY0OTA4MDM2NCwiZXhwIjoxNjY0NjMyMzY0LCJpYXQiOjE2NDkwODAzNjR9.lbT5SM_ZUaRDuX-YmGRWByFQpenWBYWTWUFbRncjifQ",
