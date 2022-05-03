805ac737c28b0e06fc04f132c5472b5219733899
yoj9168@gmail.com <yoj9168@gmail.com>
2022-05-03 16:50:45 +0900

    rating complete

--- indexJs.js
+++ indexJs.js
@@ -1,71 +1,71 @@
-var accessId;
-var matchtype, division, date;
-function get_matchType(matchtype){
-    $.ajax({
-        method:"GET",
-        url:"https://static.api.nexon.co.kr/fifaonline4/latest/matchtype.json",
-        success:function(response){
-            for(i = 0; i < response.length; i++){
-                if(response[i].matchtype == matchtype){
-                    console.log(response[i].desc);
-                }
-            }
+var matchtype, division;
+
+fetch("https://static.api.nexon.co.kr/fifaonline4/latest/matchtype.json")
+.then(res => res.json())
+.then(json => {
+    matchtype = json;
+});
+
+fetch("https://static.api.nexon.co.kr/fifaonline4/latest/division.json")
+.then(res => res.json())
+.then(json => {
+    division = json;
+});
+
+function get_desc(matchType){
+    for(i = 0; i < matchtype.length; i++){
+        if(matchType == matchtype[i].matchtype)
+            //console.log(matchtype[i].desc);
+            return matchtype[i].desc;
         }
-    })
 }
-function get_division(division){
-    $.ajax({
-        method:"GET",
-        url:"https://static.api.nexon.co.kr/fifaonline4/latest/division.json",
-        success:function(response){
-            for(i = 0; i < response.length; i++){
-                if(response[i].divisionId == division){
-                    console.log(response[i].divisionName);
-                }
-            }
-        }
-    })
+function get_divisionName(diVison){
+    console.log(diVison);
+    console.log(division);
+    for(i = 0; i < division.length; i++){
+        if(diVison === division[i].divisionId)
+            return division[i].divisionName;
+    }
 }
+
 $('.submit').click(function(){
-    var nickname;
-    //nickname = $('.input').val();
-    nickname = "5호선파브르"
-    $.ajax({
-        method:"GET",
-        url:"https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname="+nickname,
-        beforeSend:function(xhr){
-            xhr.setRequestHeader("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMjY4OTE3Mjk2IiwiYXV0aF9pZCI6IjIiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4iLCJzZXJ2aWNlX2lkIjoiNDMwMDExNDgxIiwiWC1BcHAtUmF0ZS1MaW1pdCI6IjUwMDoxMCIsIm5iZiI6MTY0OTA4MDM2NCwiZXhwIjoxNjY0NjMyMzY0LCJpYXQiOjE2NDkwODAzNjR9.lbT5SM_ZUaRDuX-YmGRWByFQpenWBYWTWUFbRncjifQ");
+    //var nickname;
+    var nickname = $('.input').val();
+    //nickname = "5호선파브르"
+    fetch("https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname="+nickname,{
+        method:'GET',
+        headers:{"Authorization":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMjY4OTE3Mjk2IiwiYXV0aF9pZCI6IjIiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4iLCJzZXJ2aWNlX2lkIjoiNDMwMDExNDgxIiwiWC1BcHAtUmF0ZS1MaW1pdCI6IjUwMDoxMCIsIm5iZiI6MTY0OTA4MDM2NCwiZXhwIjoxNjY0NjMyMzY0LCJpYXQiOjE2NDkwODAzNjR9.lbT5SM_ZUaRDuX-YmGRWByFQpenWBYWTWUFbRncjifQ",
         },
-        success: function(response){
-            $('.nickname').text("nickname : " + response.nickname);
-            $('.level').text("level  : " + response.level);
-            accessId = response.accessId;
-            console.log(accessId);
-        },
-        error: function(reponse){
-            alert(reponse.responseJSON.message);
-        }
-    });
+    })
+    .then(res=>res.json())
+    .then(data =>{
+        $('.nickname').text("nickname : " + data.nickname);
+        $('.level').text("level  : " + data.level);
+        accessId = data.accessId;
+        console.log(accessId);
+    })
+    .catch((error)=> {
+        alert(error);
+    })
 });
 $('.rating').click(function(){
-    $.ajax({
-        method:"GET",
-        url:"https://api.nexon.co.kr/fifaonline4/v1.0/users/"+accessId+"/maxdivision",
-        beforeSend:function(xhr){
-            xhr.setRequestHeader("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMjY4OTE3Mjk2IiwiYXV0aF9pZCI6IjIiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4iLCJzZXJ2aWNlX2lkIjoiNDMwMDExNDgxIiwiWC1BcHAtUmF0ZS1MaW1pdCI6IjUwMDoxMCIsIm5iZiI6MTY0OTA4MDM2NCwiZXhwIjoxNjY0NjMyMzY0LCJpYXQiOjE2NDkwODAzNjR9.lbT5SM_ZUaRDuX-YmGRWByFQpenWBYWTWUFbRncjifQ");
+    fetch("https://api.nexon.co.kr/fifaonline4/v1.0/users/"+accessId+"/maxdivision", {
+        method:'GET',
+        headers:{
+            "Authorization":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMjY4OTE3Mjk2IiwiYXV0aF9pZCI6IjIiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4iLCJzZXJ2aWNlX2lkIjoiNDMwMDExNDgxIiwiWC1BcHAtUmF0ZS1MaW1pdCI6IjUwMDoxMCIsIm5iZiI6MTY0OTA4MDM2NCwiZXhwIjoxNjY0NjMyMzY0LCJpYXQiOjE2NDkwODAzNjR9.lbT5SM_ZUaRDuX-YmGRWByFQpenWBYWTWUFbRncjifQ",
         },
-        success: function(response){
-            for(i = 0; i < response.length; i++){
-                //console.log(response[i]);
-                get_matchType(response[i].matchType);
-                get_division(response[i].division);
-                console.log(response[i].achievementDate);
-            }
-        },
-        error: function(reponse){
-            alert(reponse.responseJSON.message);
-        }
-    });
-});
-
-
+    })
+    .then(res => res.json())
+    .then(data => {
+        //console.log(data);
+        $('.matchtype0').text("경기 종류 : "+ get_desc(data[0].matchType));
+        $('.division0').text("최고 티어 : "+ get_divisionName(data[0].division));
+        $('.date0').text("달성 날짜 : "+ data[0].achievementDate);
+        $('.matchtype1').text("경기 종류 : "+ get_desc(data[1].matchType));
+        $('.division1').text("최고 티어 : "+ get_divisionName(data[1].division));
+        $('.date1').text("달성 날짜 : "+ data[1].achievementDate);
+    })
+    .catch((error)=>{
+        alert(error);
+    })
+})
